9ecaa6f41e0ec503402d669cbffb078cdc5acaab
Andrei Gusinskii <andrey@profadmin.biz>
2022-10-24 16:12:49 +0400

    Done

---
+++ weather.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+import {getArgs} from './helpers/args.js';
+import {printHelp, printSuccess, printError, printWeather} from "./services/log.services.js";
+import {getKeyValue, saveKeyValue, TOKEN_DICTIONARY} from "./services/storage.service.js";
+import {getIcon, getWeather} from "./services/api.service.js";
+
+const saveToken = async (token) => {
+  if (!token.length) {
+    printError('Не передан токен!');
+    return;
+  }
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.token, token);
+    printSuccess('Токен сохранен');
+  } catch (e) {
+    printError(e.message);
+  }
+}
+
+const saveCity = async (city) => {
+  if (!city.length) {
+    printError('Не передан город!');
+    return;
+  }
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.city, city);
+    printSuccess('Город сохранен');
+  } catch (e) {
+    printError(e.message);
+  }
+}
+
+const getForcast = async () => {
+  try {
+    const city = await getKeyValue(TOKEN_DICTIONARY.city);
+    const weather = await getWeather(city);
+    printWeather(weather,getIcon(weather.weather[0].icon))
+  } catch (e) {
+    if (e?.response?.status === 404) {
+      printError('Не верно указан город.');
+    } else if (e?.response?.status === 401) {
+      printError('Не вер но указан токен.')
+    } else {
+      printError(e.message);
+    }
+  }
+}
+const initCLI = () => {
+  const args = getArgs(process.argv);
+  if (args.h) {
+    return printHelp();
+  }
+  if (args.s) {
+    return saveCity(args.s);
+  }
+  if (args.t) {
+    return saveToken(args.t);
+  }
+  return getForcast();
+};
+
+initCLI()
